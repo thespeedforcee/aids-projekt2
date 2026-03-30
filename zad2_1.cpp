a0bc43cf602752af59d6e6277bf0be592ceacf71
maryhahahadot <linkowska.mania@gmail.com>
2026-03-30 14:45:33 +0200

    Add files via upload

--- zad2_1.cpp
+++ zad2_1.cpp
@@ -3,50 +3,58 @@
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <iomanip>
 
 using namespace std;
 
 long long porownania = 0;
 
 struct Wezel {
-    int wartosc;
-    Wezel* lewy;
+	int wartosc;
+	Wezel* lewy;
 	Wezel* prawy;
 
-    Wezel(int x) { //tworzy lisc bez dzieci
-        wartosc = x;
-        lewy = nullptr;
-        prawy = nullptr;
-    }
+	Wezel(int x) { //tworzy lisc bez dzieci
+		wartosc = x;
+		lewy = nullptr;
+		prawy = nullptr;
+	}
 };
 
 //budowa drzewa, mniejsze lewo, wieksze prawo
 Wezel* wstaw(Wezel* korzen, int x) {
-    // jesli miejsce jest puste, zrob nowy wezel
-    if (!korzen) {
-    	return new Wezel(x);
+	// jesli miejsce jest puste, zrob nowy wezel
+	if (!korzen) {
+		return new Wezel(x);
 	}
-    porownania++;
-    // jesli x jest mniejszy od wartosci obecnego wez³a
-    if (x < korzen->wartosc) {
-    	korzen->lewy = wstaw(korzen->lewy, x);
-    } 
-    //jesli x jest wiekszy
-    else if (x > korzen->wartosc) {
-        korzen->prawy = wstaw(korzen->prawy, x);
-    }
-    return korzen; // Jeœli x jest równy, nic nie robimy 
+	porownania++;
+	// jesli x jest mniejszy od wartosci obecnego wez³a
+	if (x < korzen->wartosc) {
+		korzen->lewy = wstaw(korzen->lewy, x);
+	}
+	//jesli x jest wiekszy
+	else if (x > korzen->wartosc) {
+		korzen->prawy = wstaw(korzen->prawy, x);
+	}
+	return korzen; // Jeœli x jest równy, nic nie robimy
 }
 
-int podajwys(Wezel* wezel){ //sprawdza glebokosc drzewa
-	if (!wezel){
+int podajwys(Wezel* wezel) { //sprawdza glebokosc drzewa
+	if (!wezel) {
 		return -1;
 	}
 	return 1 + max(podajwys(wezel->lewy), podajwys(wezel->prawy));
 }
 
-void wyswietl(Wezel* wezel, int wciecie = 0){
-	if (!wezel){
+int liczwezly(Wezel* wezel) {
+	if (!wezel) {
+		return 0;
+	}
+	return 1 + liczwezly(wezel->lewy) + liczwezly(wezel->prawy);
+}
+
+void wyswietl(Wezel* wezel, int wciecie = 0) {
+	if (!wezel) {
 		return;
 	}
 	wciecie += 4;
@@ -55,13 +63,13 @@ void wyswietl(Wezel* wezel, int wciecie = 0){
 	wyswietl(wezel->lewy, wciecie);
 }
 
-void znajdzmin(Wezel* wezel){
+void znajdzmin(Wezel* wezel) {
 	cout << "min: ";
 	int c = 0;
-	while(wezel){
+	while(wezel) {
 		cout << wezel->wartosc << " ";
 		c++;
-		if(!wezel->lewy){
+		if(!wezel->lewy) {
 			break;
 		}
 		wezel = wezel->lewy;
@@ -69,13 +77,13 @@ void znajdzmin(Wezel* wezel){
 	cout << "\nPorownania: " << c << endl;
 }
 
-void znajdzmax(Wezel* wezel){
+void znajdzmax(Wezel* wezel) {
 	cout << "max: ";
 	int c = 0;
-	while(wezel){
+	while(wezel) {
 		cout << wezel->wartosc << " ";
 		c++;
-		if(!wezel->prawy){
+		if(!wezel->prawy) {
 			break;
 		}
 		wezel = wezel->prawy;
@@ -83,8 +91,8 @@ void znajdzmax(Wezel* wezel){
 	cout << "\nPorownania: " << c << endl;
 }
 
-Wezel* znajdz(Wezel* wezel, int x, int level, int &znalezionolevel){
-	if (!wezel){
+Wezel* znajdz(Wezel* wezel, int x, int level, int &znalezionolevel) {
+	if (!wezel) {
 		return nullptr;
 	}
 	porownania++;
@@ -99,11 +107,11 @@ Wezel* znajdz(Wezel* wezel, int x, int level, int &znalezionolevel){
 	}
 }
 
-void wypiszlevel(Wezel* wezel, int target, int teraz = 0){
-	if (!wezel){
+void wypiszlevel(Wezel* wezel, int target, int teraz = 0) {
+	if (!wezel) {
 		return;
 	}
-	if (teraz == target){
+	if (teraz == target) {
 		cout << wezel->wartosc << " ";
 		return;
 	}
@@ -111,8 +119,8 @@ void wypiszlevel(Wezel* wezel, int target, int teraz = 0){
 	wypiszlevel(wezel->prawy, target, teraz+1);
 }
 
-void preorder(Wezel* wezel){
-	if (!wezel){
+void preorder(Wezel* wezel) {
+	if (!wezel) {
 		return;
 	}
 	cout << wezel->wartosc << " ";
@@ -120,8 +128,8 @@ void preorder(Wezel* wezel){
 	preorder(wezel->prawy);
 }
 
-void odkonca(Wezel* wezel){
-	if (!wezel){
+void odkonca(Wezel* wezel) {
+	if (!wezel) {
 		return;
 	}
 	odkonca(wezel->prawy);
@@ -129,142 +137,296 @@ void odkonca(Wezel* wezel){
 	odkonca(wezel->lewy);
 }
 
-Wezel* znajdzminwezel(Wezel* wezel){
-	while (wezel && wezel->lewy){
+Wezel* znajdzminwezel(Wezel* wezel) {
+	while (wezel && wezel->lewy) {
+		porownania++;
 		wezel = wezel->lewy;
 	}
 	return wezel;
 }
 
-Wezel* usun(Wezel* wezel, int x){
-	if (!wezel){
-		return nullptr;
+void usunPoddrzewo(Wezel* wezel) {
+	if (!wezel) {
+		return;
 	}
-	if (x < wezel->wartosc){
-		wezel->lewy = usun(wezel->lewy, x);
+	usunPoddrzewo(wezel->lewy);
+	usunPoddrzewo(wezel->prawy);
+	delete wezel;
+}
+
+Wezel* usunZPoddrzewem(Wezel* wezel, int x, bool calepoddrzewo) {
+	if (!wezel) return nullptr;
+
+	if (x < wezel->wartosc) {
+		wezel->lewy = usunZPoddrzewem(wezel->lewy, x, calepoddrzewo);
 	} else if (x > wezel->wartosc) {
-		wezel->prawy = usun(wezel->prawy, x);
+		wezel->prawy = usunZPoddrzewem(wezel->prawy, x, calepoddrzewo);
 	} else {
-		if (!wezel->lewy){
+		// USUÑ CA£E PODDRZEWO
+		if (calepoddrzewo) {
+			usunPoddrzewo(wezel);
+			return nullptr;
+		}
+
+		// standardowe usuwanie
+		if (!wezel->lewy) {
 			Wezel* tmp = wezel->prawy;
 			delete wezel;
 			return tmp;
 		}
-		if (!wezel->prawy){
+		if (!wezel->prawy) {
 			Wezel* tmp = wezel->lewy;
 			delete wezel;
 			return tmp;
 		}
+
 		Wezel* tmp = znajdzminwezel(wezel->prawy);
 		wezel->wartosc = tmp->wartosc;
-		wezel->prawy = usun(wezel->prawy, tmp->wartosc);
+		wezel->prawy = usunZPoddrzewem(wezel->prawy, tmp->wartosc, false);
 	}
 	return wezel;
 }
 
+Wezel* rotujPrawo(Wezel* wezel) {
+	Wezel* nowy = wezel->lewy;
+	wezel->lewy = nowy->prawy;
+	nowy->prawy = wezel;
+	return nowy;
+}
+
+Wezel* rotujLewo(Wezel* wezel) {
+	Wezel* nowy = wezel->prawy;
+	wezel->prawy = nowy->lewy;
+	nowy->lewy = wezel;
+	return nowy;
+}
+
+Wezel* stworzVine(Wezel* korzen) {
+	Wezel dummy(0);
+	dummy.prawy = korzen;
+
+	Wezel* ogon = &dummy;
+	Wezel* reszta = korzen;
+
+	while (reszta) {
+		if (reszta->lewy) {
+			reszta = rotujPrawo(reszta);
+			ogon->prawy = reszta;
+		} else {
+			ogon = reszta;
+			reszta = reszta->prawy;
+		}
+	}
+	return dummy.prawy;
+}
+
+Wezel* kompresuj(Wezel* korzen, int count) {
+	Wezel dummy(0);
+	dummy.prawy = korzen;
+	Wezel* tmp = &dummy;
+
+	for (int i = 0; i < count; i++) {
+		Wezel* child = tmp->prawy;
+		if (!child) break;
+
+		Wezel* newChild = rotujLewo(child);
+		tmp->prawy = newChild;
+		tmp = newChild;
+	}
+	return dummy.prawy;
+}
+
+Wezel* DSW(Wezel* korzen) {
+	// 1. vine
+	korzen = stworzVine(korzen);
+
+	// 2. liczba wêz³ów
+	int n = liczwezly(korzen);
+
+	// 3. najwiêksza potêga 2 <= n+1
+	int m = 1;
+	while (m <= n) m <<= 1;
+	m = m / 2 - 1;
+
+	// 4. pierwsza kompresja
+	korzen = kompresuj(korzen, n - m);
+
+	// 5. kolejne
+	while (m > 1) {
+		m /= 2;
+		korzen = kompresuj(korzen, m);
+	}
+
+	return korzen;
+}
+
 int main() {
-    Wezel* korzen = nullptr;
-    int wybor;
+	Wezel* korzen = nullptr;
+	int wybor;
 
-    do {
-        cout << "\n--- BST DEMO ---\n";
-        cout << "1. Utworz drzewo\n";
-        cout << "2. Wyswietl drzewo\n";
-        cout << "3. MIN / MAX\n";
-        cout << "4. Szukaj element\n";
-        cout << "5. Usun element\n";
-        cout << "6. Wypisz malejaco\n";
-        cout << "0. Wyjscie\n";
-        cout << "Wybor: ";
-        cin >> wybor;
+	do {
+		cout << "\nBST DEMO\n";
+		cout << "1. Utworz drzewo\n";
+		cout << "2. Wyswietl drzewo\n";
+		cout << "3. MIN / MAX\n";
+		cout << "4. Szukaj element\n";
+		cout << "5. Usun element\n";
+		cout << "6. Wypisz malejaco\n";
+		cout << "7. Rownowazenie DSW\n";
+		cout << "0. Wyjscie\n";
+		cout << "Wybor: ";
+		cin >> wybor;
 
-        if (wybor == 1) {
-            int n, x;
-            set<int> used;
-            korzen = nullptr;
+		if (wybor == 1) {
+			int n, x;
+			vector<int> bufor; //tymczasowe przechowanie liczb
+			set<int> used;
+			korzen = nullptr;
 
-            cout << "n <= 12 (demo): ";
-            cin >> n;
+			cout << "n <= 12 (demo): ";
+			cin >> n;
 
-            if (n > 12) {
-                cout << "Tryb demo tylko do 12!\n";
-                continue;
-            }
+			if (n > 12) {
+				cout << "Tryb demo tylko do 12!\n";
+				continue;
+			}
 
-            auto start = chrono::high_resolution_clock::now();
+			cout << "Wpisz " << n << " elementow: " << endl;
+			for (int i = 0; i < n; i++) {
+				cin >> x;
+				bufor.push_back(x);
+				used.insert(x);
+			}
+			auto start = chrono::high_resolution_clock::now();
 
-            for (int i = 0; i < n; i++) {
-                cin >> x;
+			if (used.size() != bufor.size()) {
+				cout << "Blad: Dane nie sa roznowartosciowe\n";
+			} else {
+				for (int wartosc : bufor) {
+					korzen = wstaw(korzen, wartosc);
+				}
 
-                if (used.count(x)) {
-                    cout << "Duplikat! Wpisz dane od nowa.\n";
-                    korzen = nullptr;
-                    break;
-                }
+				auto stop = chrono::high_resolution_clock::now();
 
-                used.insert(x);
-                korzen = wstaw(korzen, x);
-            }
+				cout << "Czas budowania drzewa: "
+				     << chrono::duration<double>(stop - start).count()
+				     << " \n";
 
-            auto stop = chrono::high_resolution_clock::now();
+				cout << "Wysokosc: " << podajwys(korzen) << endl;
+				cout << "Liczba wezlow: " << used.size() << endl;
+				wyswietl(korzen);
+			}
+		}
 
-            cout << "Czas: "
-                 << chrono::duration<double, micro>(stop - start).count()
-                 << " us\n";
+		else if (wybor == 2) {
+			auto start = chrono::high_resolution_clock::now();
+			if(!korzen) {
+				cout << "puste drzewo\n";
+			} else {
+				wyswietl(korzen);
+			}
+			auto stop = chrono::high_resolution_clock::now();
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+		}
 
-            cout << "Wysokosc: " << podajwys(korzen) << endl;
-            cout << "Liczba wezlow: " << used.size() << endl;
-        }
+		else if (wybor == 3) {
+			auto start = chrono::high_resolution_clock::now();
+			if (!korzen) {
+				cout << "Drzewo puste\n";
+			} else {
+				znajdzmin(korzen);
+				znajdzmax(korzen);
+			}
+			auto stop = chrono::high_resolution_clock::now();
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+		}
 
-        else if (wybor == 2) {
-            wyswietl(korzen);
-        }
+		else if (wybor == 4) {
+			int x, level = -1;
+			cout << "Szukaj: ";
+			cin >> x;
 
-        else if (wybor == 3) {
-            znajdzmin(korzen);
-            znajdzmax(korzen);
-        }
+			auto start = chrono::high_resolution_clock::now();
 
-        else if (wybor == 4) {
-            int x, level = -1;
-            cout << "Szukaj: ";
-            cin >> x;
+			porownania = 0;
+			Wezel* res = znajdz(korzen, x, 0, level);
 
-            porownania = 0;
-            Wezel* res = znajdz(korzen, x, 0, level);
+			if (!res) {
+				cout << "Nie znaleziono\n";
+			} else {
+				cout << "Poziom: " << level << endl;
 
-            if (!res) {
-                cout << "Nie znaleziono\n";
-            } else {
-                cout << "Poziom: " << level << endl;
+				cout << "Elementy na poziomie: ";
+				wypiszlevel(korzen, level);
+				cout << endl;
 
-                cout << "Elementy na poziomie: ";
-                wypiszlevel(korzen, level);
-                cout << endl;
+				cout << "Wysokosc poddrzewa: " << podajwys(res) << endl;
 
-                cout << "Wysokosc poddrzewa: " << podajwys(res) << endl;
+				cout << "Preorder poddrzewa: ";
+				preorder(res);
+				cout << endl;
+			}
 
-                cout << "Preorder poddrzewa: ";
-                preorder(res);
-                cout << endl;
-            }
+			auto stop = chrono::high_resolution_clock::now();
 
-            cout << "Porownania: " << porownania << endl;
-        }
+			cout << "Porownania: " << porownania << endl;
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+		}
 
-        else if (wybor == 5) {
-            int x;
-            cout << "Usun: ";
-            cin >> x;
-            korzen = usun(korzen, x);
-        }
+		else if (wybor == 5) {
+			int x;
+			cout << "Usun: ";
+			cin >> x;
 
-        else if (wybor == 6) {
-            odkonca(korzen);
-            cout << endl;
-        }
+			porownania = 0;
 
-    } while (wybor != 0);
+			auto start = chrono::high_resolution_clock::now();
 
-    return 0;
+			int tryb;
+			cout << "1 - tylko element\n2 - cale poddrzewo\nWybor: ";
+			cin >> tryb;
+
+			korzen = usunZPoddrzewem(korzen, x, tryb == 2);
+
+			auto stop = chrono::high_resolution_clock::now();
+			cout << "Porownania: " << porownania << endl;
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+
+		}
+
+		else if (wybor == 6) {
+			auto start = chrono::high_resolution_clock::now();
+			odkonca(korzen);
+			cout << endl;
+			auto stop = chrono::high_resolution_clock::now();
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+		}
+
+		else if (wybor == 7) {
+			if (!korzen) {
+				cout << "Drzewo puste\n";
+				continue;
+			}
+
+			cout << "Preorder PRZED:\n";
+			preorder(korzen);
+			cout << endl;
+
+			auto start = chrono::high_resolution_clock::now();
+
+			korzen = DSW(korzen);
+
+			auto stop = chrono::high_resolution_clock::now();
+
+			cout << "Preorder PO:\n";
+			preorder(korzen);
+			cout << endl;
+
+			cout << "Nowa wysokosc: " << podajwys(korzen) << endl;
+			cout << "Czas: " << chrono::duration<double>(stop - start).count() << endl;
+		}
+
+	} while (wybor != 0);
+
+	return 0;
 }
